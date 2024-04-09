f6575aeb09119d9506e506bf9ff46574fa7a852a
dlv tlm <dlvtabh23@gmail.com>
2024-04-09 20:42:41 +0300

    invoice shits

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,10 +1,5 @@
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-//for the date time
-//LocalDateTime time = LocalDateTime.now();
-//DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm");
-//String rightNow = time.format(timeFormat);
 
 public class Invoice {
     public Order order;
@@ -17,5 +12,17 @@ public class Invoice {
 
     public double calculateProfit(){
         return (order.getProduct().calculateProductProfit()) * order.getQuantity();
+    }
+    public void invoiceFormatForCustomer(Order order) {
+       StringBuffer invoice = new StringBuffer();
+       int quantity = order.getQuantity();
+       double price = order.getProduct().getSellPrice()*quantity;
+       double tax = price*0.17;
+        invoice.append(order.toString()).append("\n").append("price without tax:").append(price * 0.83).append("\n").append("total taxes:").append(tax);
+        System.out.print(invoice);
+    }
+    public void invoiceFormatForAccountant(Order order){
+        invoiceFormatForCustomer(order);
+        System.out.println("profit: " + calculateProfit());
     }
 }
